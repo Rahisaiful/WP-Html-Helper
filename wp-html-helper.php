d0b362a4927e6c28e40912c9c08a282361882c42
Rahisaiful <rahigm6039@gmail.com>
2019-01-18 00:34:33 +0600

    Add wrap_before and wrap_after argument

--- wp-html-helper.php
+++ wp-html-helper.php
@@ -6,8 +6,8 @@
  */
 
 // image default alt
-if( !function_exists( 'img_default_alt' ) ){
-	function img_default_alt( $url = '' ){
+if( ! function_exists( 'prefix_img_default_alt' ) ) {
+	function prefix_img_default_alt( $url = '' ) {
 
 		if( $url != '' ){
 			// attachment id by url 
@@ -34,8 +34,8 @@ if( !function_exists( 'img_default_alt' ) ){
 
 
 // Image Tag
-if( !function_exists( 'img_tag' ) ){
-	function img_tag( array $args ){
+if( ! function_exists( 'prefix_img_tag' ) ) {
+	function prefix_img_tag( array $args ) {
 		
 		
 		$default = array(
@@ -54,7 +54,7 @@ if( !function_exists( 'img_tag' ) ){
 		$url = $args['url'];
 
 		// image tag alter
-		if( !empty( $args['alt'] ) ){
+		if( ! empty( $args['alt'] ) ) {
 			$alt = $args['alt'];
 		}else{
 			$alt = img_default_alt( $url );
@@ -66,41 +66,44 @@ if( !function_exists( 'img_tag' ) ){
 		
 		$attr = '';
 		// Image class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// Image id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 		// Image width
-		if( !empty( $args['width'] ) ){
+		if( ! empty( $args['width'] ) ) {
 			$attr .= ' width="'.esc_attr( $args['width'] ).'"';
 		}
 		// Image height
-		if( !empty( $args['height'] ) ){
+		if( ! empty( $args['height'] ) ) {
 			$attr .= ' height="'.esc_attr( $args['height'] ).'"';
 		}
 		// Image srcset
-		if( !empty( $args['srcset'] ) ){
+		if( ! empty( $args['srcset'] ) ) {
 			$attr .= ' srcset="'.esc_attr( $args['srcset'] ).'"';
 		}
 		
-		
-		echo '<img src="'.esc_url( $url ).'" alt="'.esc_attr( $alt ).'" '.wp_kses_post( $attr ).' />';
+		return '<img src="' . esc_url( $url ) . '" alt="' . esc_attr( $alt ) . '" ' . wp_kses_post( $attr ) . ' />';
+
 	}
 }
 
 // Anchor Tag
-if( !function_exists( 'anchor_tag' ) ){
-	function anchor_tag( array $args ){
+if( ! function_exists( 'prefix_anchor_tag' ) ) {
+	function prefix_anchor_tag( array $args ) {
 		
 		$default = array(
 			'url' 	 => '',
 			'text' 	 => 'Click Here',
 			'target' => '',
+			'title'  => '',
 			'class'  => '',
 			'id' 	 => '',
+			'wrap_before' 	=> '',
+			'wrap_after' 	=> '',
 		);
 		
 		$args = wp_parse_args( $args,  $default );
@@ -118,34 +121,52 @@ if( !function_exists( 'anchor_tag' ) ){
 		
 		$attr = '';
 		// class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 		// target 
-		if( !empty( $args['target'] ) ){
+		if( ! empty( $args['target'] ) ) {
 			$attr .= ' target="'.esc_attr( $args['target'] ).'"';
 		}
+		// Title 
+		if( ! empty( $args['title'] ) ) {
+			$attr .= ' title="' . esc_attr( $args['title'] ) . '"';
+		}
 
+		$data = '';
 		
-		echo '<a href="'.esc_url( $url ).'" '.wp_kses_post( $attr ).'>'.esc_html( $text ).'</a>';
+		// Wrapper Start
+		if( ! empty( $args['wrap_before'] ) ) {
+			$data .= $args['wrap_before'];
+		}
+			$data .= '<a href="' . esc_url( $url ) . '" '. $attr.'>'.$text.'</a>';
+			
+		// Wrapper End
+		if( ! empty( $args['wrap_after'] ) ) {
+			$data .= $args['wrap_after'];
+		}
+		
+		return wp_kses_post( $data );
 		
 		
 	}
 }
 
 // Heading Tag
-if( !function_exists( 'heading_tag' ) ){
-	function heading_tag( array $args ){
+if( ! function_exists( 'prefix_heading_tag' ) ) {
+	function prefix_heading_tag( array $args ) {
 		
 		$default = array(
 			'tag' 	 => 'h1',
 			'text' 	 => 'Write Something',
 			'class'  => '',
 			'id' 	 => '',
+			'wrap_before' => '',
+			'wrap_after'  => '',
 		);
 		
 		$args = wp_parse_args( $args,  $default );
@@ -159,30 +180,43 @@ if( !function_exists( 'heading_tag' ) ){
 		
 		$attr = '';
 		// class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 
-
+		$data = '';
 		
-		echo '<'.wp_kses_post( $tag.$attr ).'>'.esc_html( $args['text'] ).'</'.esc_attr( $tag ).'>';
+		// Wrapper Start
+		if( ! empty( $args['wrap_before'] ) ) {
+			$data .= $args['wrap_before'];
+		}
+			$data .= '<' . esc_attr( $tag ) . $attr . '>' . $args['text'] . '</' . esc_attr( $tag ) . '>';
+			
+		// Wrapper End
+		if( ! empty( $args['wrap_after'] ) ) {
+			$data .= $args['wrap_after'];
+		}
+		
+		return wp_kses_post( $data );
 		
 		
 	}
 }
 
 // Paragraph Tag
-if( !function_exists( 'paragraph_tag' ) ){
-	function paragraph_tag( array $args ){
+if( ! function_exists( 'prefix_paragraph_tag' ) ) {
+	function prefix_paragraph_tag( array $args ) {
 		
 		$default = array(
 			'text' 	 => 'Write Something',
 			'class'  => '',
 			'id' 	 => '',
+			'wrap_before' => '',
+			'wrap_after'  => '',
 		);
 		
 		$args = wp_parse_args( $args,  $default );
@@ -194,30 +228,43 @@ if( !function_exists( 'paragraph_tag' ) ){
 		
 		$attr = '';
 		// class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 
+		$pdata = '';
+		
+		// Wrapper Start
+		if( ! empty( $args['wrap_before'] ) ) {
+			$pdata .= $args['wrap_before'];
+		}
+			$pdata .= '<p' . $attr . '>' . $args['text'] . '</p>';
+		// Wrapper End
+		if( ! empty( $args['wrap_after'] ) ) {
+			$pdata .= $args['wrap_after'];
+		}
 
-		echo '<p'.wp_kses_post( $attr ).'>'.wp_kses_post( $args['text'] ).'</p>';
+		return wp_kses_post( $pdata );
 		
 		
 	}
 }
 
 // Other Tag
-if( !function_exists( 'other_tag' ) ){
-	function other_tag( array $args ){
+if( ! function_exists( 'prefix_other_tag' ) ) {
+	function prefix_other_tag( array $args ) {
 		
 		$default = array(
 			'tag' 	 => 'span',
 			'text' 	 => 'Write Something',
 			'class'  => '',
 			'id' 	 => '',
+			'wrap_before' => '',
+			'wrap_after'  => '',
 		);
 		
 		$args = wp_parse_args( $args,  $default );
@@ -231,31 +278,44 @@ if( !function_exists( 'other_tag' ) ){
 		
 		$attr = '';
 		// class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 
 
+		$tagdata = '';
 		
-		echo '<'.wp_kses_post( $tag.$attr ).'>'.esc_html( $args['text'] ).'</'.esc_attr( $tag ).'>';
+		// Button Wrapper Start
+		if( ! empty( $args['wrap_before'] ) ) {
+			$tagdata .= $args['wrap_before'];
+		}
+		$tagdata .= '<' . esc_attr( $tag ) . $attr . '>' . $args['text'] . '</' . esc_attr( $tag ) . '>';
+		// Button Wrapper End
+		if( ! empty( $args['wrap_after'] ) ) {
+			$tagdata .= $args['wrap_after'];
+		}
+		
+		return wp_kses_post( $tagdata );
 		
 		
 	}
 }
 
 // Button Element 
-if( !function_exists( 'button_element ' ) ){
-	function button_element( array $args ){
+if( ! function_exists( 'prefix_button_element ' ) ) {
+	function prefix_button_element( array $args ) {
 		
 		$default = array(
 			'text' 	 => 'Button',
 			'type'   => '',
 			'class'  => '',
 			'id' 	 => '',
+			'wrap_before' => '',
+			'wrap_after'  => ''
 		);
 		
 		$args = wp_parse_args( $args,  $default );
@@ -266,25 +326,31 @@ if( !function_exists( 'button_element ' ) ){
 		
 		$attr = '';
 		// class 
-		if( !empty( $args['class'] ) ){
+		if( ! empty( $args['class'] ) ) {
 			$attr .= ' class="'.esc_attr( $args['class'] ).'"';
 		}
 		// id 
-		if( !empty( $args['id'] ) ){
+		if( ! empty( $args['id'] ) ) {
 			$attr .= ' id="'.esc_attr( $args['id'] ).'"';
 		}
 		// type 
-		if( !empty( $args['type'] ) ){
+		if( ! empty( $args['type'] ) ) {
 			$attr .= ' type="'.esc_attr( $args['type'] ).'"';
 		}
 
-
-		echo '<button '.wp_kses_post( $attr ).'>'.esc_html( $args['text'] ).'</button>';
+		$btn = '';
+		
+		// Button Wrapper Start
+		if( ! empty( $args['wrap_before'] ) ) {
+			$btn .= $args['wrap_before'];
+		}
+		$btn .= '<button ' . $attr . '>' . $args['text'] . '</button>';
+		// Button Wrapper End
+		if( ! empty( $args['wrap_after'] ) ) {
+			$btn .= $args['wrap_after'];
+		}
+		
+		return wp_kses_post( $btn );
 		
 	}
 }
-
-
-
-
-?>
